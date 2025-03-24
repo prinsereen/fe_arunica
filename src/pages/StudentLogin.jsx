266ca9267e2fd876c1393cddce5b3b50a5e4b499
prinsereen <prinsnnuzeren@gmail.com>
2025-03-24 12:20:18 +0700

    fix register login

--- src/pages/StudentLogin.jsx
+++ src/pages/StudentLogin.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 
 const StudentLogin = () => {
-        const [formData, setFormData] = useState({
+    const [formData, setFormData] = useState({
         nisn: '',
         password: '',
     });
@@ -24,9 +24,9 @@ const StudentLogin = () => {
         e.preventDefault();
         try {
             const response = await api.login(formData);
-            console.log(response)
+            console.log(response);
             if (response.accessToken) {
-                api.putAccessToken(response.accessToken)
+                api.putAccessToken(response.accessToken);
                 navigate('/');
             } else {
                 const errorData = await response.json();
@@ -38,72 +38,68 @@ const StudentLogin = () => {
     };
 
     return (
-        <>
-            <div className='min-h-screen grid grid-cols-2'>
-                <div className="bg-white flex-grow">
-                    <h2 className="text-center mt-28 text-3xl font-bold">Sign In</h2>
-                    <form className="space-y-3" onSubmit={handleSubmit} action="">
-                    <div className="mx-24 mt-4">
-                        <label className="block text-sm text-[#515151] font-bold">NISN</label>
-                        <input
-                            type="text"
-                            className="mt-1 block w-full px-3 py-1 border bg-[#E8E8E8] border-black rounded-full shadow-sm focus:outline-none focus:ring-black focus:border-black"
-                            name="nisn"
-                            placeholder="Masukan NISN Anda"
-                            value={formData.nisn}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className="mx-24 ">
-                        <label className="block text-sm text-[#515151] font-bold">Passsword</label>
-                        <input
-                            type="password"
-                            className="mt-1 block w-full px-3 py-1 border bg-[#E8E8E8] border-black rounded-full shadow-sm focus:outline-none focus:ring-black focus:border-black"
-                            name="password"
-                            placeholder="Masukan password Anda"
-                            value={formData.password}
-                            onChange={handleChange}
-                        />
-                    </div>
-                    <div className="flex mx-24 items-center justify-between">
-                        <button type="submit" className=" mt-4 w-full bg-[#1450A3] text-white px-4 py-2 rounded-full hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500">
+        <div className='min-h-screen flex flex-col md:grid md:grid-cols-2'>
+            <div className="bg-white flex items-center justify-center p-6">
+                <div className="w-full max-w-md">
+                    <h2 className="text-center text-3xl font-bold mb-6">Sign In</h2>
+                    <form className="space-y-4" onSubmit={handleSubmit}>
+                        <div>
+                            <label className="block text-sm text-[#515151] font-bold">NISN</label>
+                            <input
+                                type="text"
+                                className="mt-1 block w-full px-3 py-2 border bg-[#E8E8E8] border-black rounded-full focus:outline-none focus:ring-black focus:border-black"
+                                name="nisn"
+                                placeholder="Masukan NISN Anda"
+                                value={formData.nisn}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <div>
+                            <label className="block text-sm text-[#515151] font-bold">Password</label>
+                            <input
+                                type="password"
+                                className="mt-1 block w-full px-3 py-2 border bg-[#E8E8E8] border-black rounded-full focus:outline-none focus:ring-black focus:border-black"
+                                name="password"
+                                placeholder="Masukan password Anda"
+                                value={formData.password}
+                                onChange={handleChange}
+                            />
+                        </div>
+                        <button type="submit" className="w-full bg-[#1450A3] text-white px-4 py-2 rounded-full hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500">
                             Sign In
                         </button>
-                    </div>
                     </form>
-                    <p className="mt-4 flex mx-24 items-center justify-center">
+                    <p className="mt-4 text-center">
                         Belum memiliki akun? <Link to="/register/siswa" className="text-[#1450A3] underline hover:text-indigo-800">Register</Link>
                     </p>
                 </div>
-                <div className="bg-[#F94C10] pl-2 rounded-l-3xl flex flex-col">
-                    <div className="bg-[#EFD595] rounded-l-3xl flex-grow">
-                        <div className='flex flex-col items-center'>
-                            <img className="mt-8 w-[60px]" src={logo} alt="logo" />
-                            <h1 className="text-2xl font-semibold">Selamat Datang di Arundemy</h1>
-                        </div>
-                        <div className='flex mt-8'>
-                            <p className="mt-12 ml-4 font-semibold bg-[#91C8E4] rounded-l-lg rounded-b-lg h-[90px] p-2">
-                                Selamat Datang Kembali<br />
-                                Tantangan baru menantimu !!! <br />
-                                 🥰🤩
-                            </p>
-                            <img className="w-[280px]" src={maskot} alt="maskot" />
-                        </div>
-                        <div className='flex flex-col items-center py-4'>
-                            <p>Temukan Arundemy di</p>
-                            <div className='flex items-center'>
-                                <div className="p-2 z-40 rounded-full bg-white">
-                                    <img className="w-[25px]" src={instagram} alt="instagram" />
-                                </div>
-                                <div className="z-30 p-1 -translate-x-2 bg-white rounded-r-full">
-                                    <p className="px-2 bg-[#E8E8E8] rounded-r-full">arundemy</p>
-                                </div>
+            </div>
+            <div className="hidden md:flex flex-col bg-[#F94C10] rounded-l-3xl justify-center items-center pl-6">
+                <div className="bg-[#EFD595] rounded-3xl w-full h-full flex flex-col items-center justify-center p-6">
+                    <img className="w-[60px]" src={logo} alt="logo" />
+                    <h1 className="text-2xl font-semibold mt-4 text-center">Selamat Datang di Arundemy</h1>
+                    <div className='flex mt-8'>
+                        <p className="font-semibold bg-[#91C8E4] h-[110px] rounded-lg p-4 text-center">
+                            Selamat Datang Kembali<br />
+                            Tantangan baru menantimu !!! <br />
+                            🥰🤩
+                        </p>
+                        <img className="w-[200px] ml-4" src={maskot} alt="maskot" />
+                    </div>
+                    <div className='flex flex-col items-center py-4'>
+                        <p>Temukan Arundemy di</p>
+                        <div className='flex items-center mt-2'>
+                            <div className="p-2 rounded-full translate-x-3 bg-white">
+                                <img className="w-[25px]" src={instagram} alt="instagram" />
+                            </div>
+                            <div className="p-2 bg-white rounded-r-full">
+                                <p className="px-2 bg-[#E8E8E8] rounded-r-full">arundemy</p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </>
+        </div>
     );
 }
 
--- src/pages/StudentRegister.jsx
+++ src/pages/StudentRegister.jsx
@@ -38,105 +38,57 @@ const StudentRegister = () => {
     };
 
     return (
-        <>
-            <div className='min-h-screen grid grid-cols-2'>
-                <div className="bg-[#F94C10] pr-2 rounded-r-3xl flex flex-col">
-                    <div className="bg-[#EFD595] rounded-r-3xl flex-grow">
-                        <div className='flex flex-col items-center'>
-                            <img className="mt-8 w-[60px]" src={logo} alt="logo" />
-                            <h1 className="text-2xl font-semibold">Selamat Datang di Arundemy</h1>
-                        </div>
-                        <div className='flex mt-8'>
-                            <img className="w-[280px]" src={maskot} alt="maskot" />
-                            <p className="mt-12 ml-4 font-semibold bg-[#91C8E4] rounded-r-lg rounded-b-lg h-[90px] p-2">
-                                Halo aku Arun,<br />
-                                Aku akan menemanimu <br />
-                                menjelajah dunia arunica 🥰🤩
-                            </p>
-                        </div>
-                        <div className='flex flex-col items-center py-4'>
-                            <p>Temukan Arundemy di</p>
-                            <div className='flex items-center'>
-                                <div className="p-2 z-40 rounded-full bg-white">
-                                    <img className="w-[25px]" src={instagram} alt="instagram" />
-                                </div>
-                                <div className="z-30 p-1 -translate-x-2 bg-white rounded-r-full">
-                                    <p className="px-2 bg-[#E8E8E8] rounded-r-full">arundemy</p>
-                                </div>
+        <div className='min-h-screen flex flex-col md:grid md:grid-cols-2'>
+            <div className="bg-[#F94C10] pr-2 md:rounded-r-3xl flex flex-col">
+                <div className="bg-[#EFD595] md:rounded-r-3xl flex-grow flex flex-col items-center p-4">
+                    <img className="mt-12 w-[50px] md:w-[60px]" src={logo} alt="logo" />
+                    <h1 className="text-xl md:text-2xl font-semibold text-center">Selamat Datang di Arundemy</h1>
+                    <div className='flex flex-col items-center mt-6 md:flex-row md:mt-8'>
+                        <img className="w-[200px] md:w-[280px]" src={maskot} alt="maskot" />
+                        <p className="mt-4 md:mt-12 ml-0 md:ml-4 font-semibold bg-[#91C8E4] rounded-lg p-3 text-center md:text-left">
+                            Halo aku Arun,<br />
+                            Aku akan menemanimu <br />
+                            menjelajah dunia arunica 🥰🤩
+                        </p>
+                    </div>
+                    <div className='flex flex-col items-center py-4'>
+                        <p>Temukan Arundemy di</p>
+                        <div className='flex items-center'>
+                            <div className="p-2 rounded-full bg-white">
+                                <img className="w-[20px] md:w-[25px]" src={instagram} alt="instagram" />
+                            </div>
+                            <div className="p-1 -translate-x-2 bg-white rounded-r-full">
+                                <p className="px-2 bg-[#E8E8E8] rounded-r-full">arundemy</p>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="bg-white flex-grow">
-                    <h2 className="text-center mt-12 text-3xl font-bold">Registrasi</h2>
-                    <form className="space-y-3" onSubmit={handleSubmit}>
-                        <div className="mx-24 mt-4">
-                            <label className="block text-sm text-[#515151] font-bold">NISN</label>
-                            <input
-                                type="text"
-                                className="mt-1 block w-full px-3 py-1 border bg-[#E8E8E8] border-black rounded-full shadow-sm focus:outline-none focus:ring-black focus:border-black"
-                                name="nisn"
-                                placeholder="Masukan NISN Anda"
-                                value={formData.nisn}
-                                onChange={handleChange}
-                            />
-                        </div>
-                        <div className="mx-24 ">
-                            <label className="block text-sm text-[#515151] font-bold">Email</label>
-                            <input
-                                type="text"
-                                className="mt-1 block w-full px-3 py-1 border bg-[#E8E8E8] border-black rounded-full shadow-sm focus:outline-none focus:ring-black focus:border-black"
-                                name="email"
-                                placeholder="Masukan Email Anda"
-                                value={formData.email}
-                                onChange={handleChange}
-                            />
-                        </div>
-                        <div className="mx-24 ">
-                            <label className="block text-sm text-[#515151] font-bold">Nama Lengkap</label>
-                            <input
-                                type="text"
-                                className="mt-1 block w-full px-3 py-1 border bg-[#E8E8E8] border-black rounded-full shadow-sm focus:outline-none focus:ring-black focus:border-black"
-                                name="name"
-                                placeholder="Masukan nama lengkap Anda"
-                                value={formData.name}
-                                onChange={handleChange}
-                            />
-                        </div>
-                        <div className="mx-24 ">
-                            <label className="block text-sm text-[#515151] font-bold">Password</label>
-                            <input
-                                type="password"
-                                className="mt-1 block w-full px-3 py-1 border bg-[#E8E8E8] border-black rounded-full shadow-sm focus:outline-none focus:ring-black focus:border-black"
-                                name="password"
-                                placeholder="Masukan password Anda"
-                                value={formData.password}
-                                onChange={handleChange}
-                            />
-                        </div>
-                        <div className="mx-24 ">
-                            <label className="block text-sm text-[#515151] font-bold">Konfirmasi Password</label>
-                            <input
-                                type="password"
-                                className="mt-1 block w-full px-3 py-1 border bg-[#E8E8E8] border-black rounded-full shadow-sm focus:outline-none focus:ring-black focus:border-black"
-                                name="conf_password"
-                                placeholder="Masukan kembali password baru Anda"
-                                value={formData.conf_password}
-                                onChange={handleChange}
-                            />
-                        </div>
-                        <div className="flex mx-24 items-center justify-between">
-                            <button type="submit" className=" mt-4 w-full bg-[#1450A3] text-white px-4 py-2 rounded-full hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500">
-                                Register
-                            </button>
-                        </div>
-                    </form>
-                    <p className="mt-4 flex mx-24 items-center justify-center">
-                        Sudah memiliki akun? <Link to="/login/siswa" className="text-[#1450A3] underline hover:text-indigo-800">Sign In</Link>
-                    </p>
-                </div>
             </div>
-        </>
+            <div className="bg-white flex-grow flex flex-col items-center p-6">
+                <h2 className="text-2xl mt-12 md:text-3xl font-bold">Registrasi</h2>
+                <form className="space-y-3 w-full max-w-md" onSubmit={handleSubmit}>
+                    {['nisn', 'email', 'name', 'password', 'conf_password'].map((field, index) => (
+                        <div key={index}>
+                            <label className="block text-sm text-[#515151] font-bold capitalize">{field.replace('_', ' ')}</label>
+                            <input
+                                type={field.includes('password') ? 'password' : 'text'}
+                                className="mt-1 block w-full px-3 py-2 border bg-[#E8E8E8] border-black rounded-lg shadow-sm focus:outline-none focus:ring-black focus:border-black"
+                                name={field}
+                                placeholder={`Masukan ${field.replace('_', ' ')} Anda`}
+                                value={formData[field]}
+                                onChange={handleChange}
+                            />
+                        </div>
+                    ))}
+                    <button type="submit" className="mt-4 w-full bg-[#1450A3] text-white px-4 py-2 rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500">
+                        Register
+                    </button>
+                </form>
+                <p className="mt-4 text-center">
+                    Sudah memiliki akun? <Link to="/login/siswa" className="text-[#1450A3] underline hover:text-indigo-800">Sign In</Link>
+                </p>
+            </div>
+        </div>
     );
 };
 
